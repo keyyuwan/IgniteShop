eed7ce77c02122f18c5f9bef065fc71e72ea726f
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-02-24 14:41:15 -0300

    style: configured stitchesjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { styled } from '@/styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+})
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Button>Enviar</Button>
     </div>
   )
 }
