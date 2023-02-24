126bb9fe1f48afd52d28ebd75582c5945b91f828
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-02-24 14:49:24 -0300

    style: defining global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { globalCss } from '.'
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    '-webkit-font-smoothing': 'antialiased',
+    backgroundColor: '$gray900',
+    color: '$gray100',
+  },
+
+  'body, input, textarea, button': {
+    font: '400 1rem Roboto, sans-serif',
+  },
+})
